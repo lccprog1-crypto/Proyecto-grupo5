2cc78e3f4c9ff0e647399c7c7322f2ad7fcea7f4
Urban20 <173964326+Urban20@users.noreply.github.com>
2026-06-10 18:28:15 -0300

    agrego carpeta de testing

---
+++ tests/test_utilidades.py
@@ -0,0 +1,8 @@
+import utilidades
+
+def test_enfermedades_prev():
+
+    tupla_test = ('PT-174479', '41', 'Female', 'Germany', 'Insulin', '10', 'Hypoglycemia', 'Moderate', 'Recovered', '2025-02-18', '2025-01-24', 'Kidney Disease', 'No', 'Frequent', 'No', '38.0', '52.52', '13.405')
+    tupla_test2 = ('PT-101653', '28', 'Male', 'USA', 'Metformin', '5', 'Diarrhea', 'Moderate', 'Recovered', '2024-01-05', '2023-11-13', '', 'Yes', '', 'No', '42.0', '38.9072', '-77.0369')
+    assert utilidades.enfermedades_prev(tupla_test) is True  # tiene enfermedades
+    assert utilidades.enfermedades_prev(tupla_test2) is False # no tiene
